fe9c69a9b48e96f0e3a5e564311051c3469a42d1
raashkobby <rashidofori11@gmail.com>
2023-12-27 08:40:02 +0000

    updates

--- routes/User.js
+++ routes/User.js
@@ -2,7 +2,7 @@ const router = require("express").Router()
 const User = require('../models/User')
 
 
-
+// find all users 
 
 router.get('/', async (req, res) => {
     try {
@@ -48,5 +48,56 @@ router.post('/login', async (req, res) => {
         res.status(500).json(error);
     }
 });
+
+
+// find a single user 
+router.get('/:id', async (req,res)=> {
+    const user = await User.findById(req.params.id)
+    try {
+        if(user){
+            res.status(200).json(user)
+        }else{
+            res.status(404).json('User not found')
+        }   
+    } catch (err) {
+       res.status(500).json(err) 
+    }
+} )
+
+
+// update single user 
+router.put('/:id', async (req, res) => {
+    try {
+      const eUser = await User.findById(req.params.id);
+      if (eUser) {
+        if (eUser._id.toString() === req.params.id) {
+          await User.updateOne({ _id: req.params.id }, { $set: req.body });
+          res.status(200).json('The user has been updated');
+        } else {
+          res.status(403).json('You cant update this user');
+        }
+      } else {
+        res.status(404).json('Post not found');
+      }
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  });
+
+
+router.delete('/:id', async (req, res)=>{
+    const exists = await User.findById(req.params.id)
+    if (exists) {
+        if (exists._id.toString()=== req.params.id) {
+            await User.deleteOne({_id: req.params.id});
+            res.status(200).json('User deleted successfully')
+        }else {
+            res.status(403).json('You cant delete this user')
+        }
+    } else {
+        res.status(404).json('User not found')
+        
+    }
+})
 
 module.exports = router;
